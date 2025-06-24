cf906bb5308af7de190ae4ffda01255705448dc7
Evan Peikon <154470963+evanpeikon@users.noreply.github.com>
2025-06-24 12:50:01 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="decypher",
+    version="1.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+        "pandas",
+        "matplotlib",
+        "seaborn",
+        "networkx",
+        "statsmodels",
+        "scipy",
+        "scikit-learn",
+        "plotly"
+    ],
+    author="Evan Peikon",
+    description="Decypher - Time series causal analysis tool with mediation analysis",
+    url="https://github.com/evanpeikon/Decypher",
+)
